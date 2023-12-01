60eee627232e4aaea6e1c672a60a5514c5bf333a
oussama saddi <oussama.saddi2@gmail.com>
2023-12-01 15:08:44 +0100

    first
    h

---
+++ src/app/etage/etage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-etage',
+  templateUrl: './etage.component.html',
+  styleUrls: ['./etage.component.css']
+})
+export class EtageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
