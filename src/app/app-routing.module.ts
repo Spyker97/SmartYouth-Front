d95db721cb6f3c142dd1d9458053ed89ca9a7eb0
Ahmed BEN HADID <ahmedbhd97@gmail.com>
2023-12-06 23:17:06 +0100

    corrigé bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashbordSoftComponent } from './dashbord-soft/dashbord-soft.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUPComponent } from './sign-up/sign-up.component';
+import { VentilateurComponent } from './ventilateur-climatiseur/ventilateur.component';
+import { AlarmeComponent } from './alarme/alarme.component';
 const routes: Routes = [
   {path:'homeDashbord', component:DashbordSoftComponent},
-  {path:'signin', component:SignInComponent}
+  {path:'signin', component:SignInComponent},
+  {path:'signup', component:SignUPComponent},
+  {path:'fan', component:VentilateurComponent},
+  {path:'alarme', component:AlarmeComponent}
 ];
 
 @NgModule({
--- src/app/ventilateur-climatiseur/ventilateur.component.ts
+++ src/app/ventilateur-climatiseur/ventilateur.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 
@@ -17,23 +17,50 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         transform: 'rotate(0deg)',
       })),
       transition('rotate <=> notRotate', animate('500ms ease-in-out')),
-    ]),
+    ])
   ]
 })
 
 export class VentilateurComponent implements OnInit {
+  @ViewChild('snowflakeIcon') snowflakeIcon!: ElementRef;
+  @ViewChild('ceilingContainer') ceilingContainer!: ElementRef;
 
-  constructor() { }
-
+  constructor(private renderer: Renderer2) { }
+  
   ngOnInit(): void {
   }
   fans = 
+    { id: 1, status: false }
+    clims = 
     { id: 1, status: false }
     // Add more fans as needed
   ;
 
   toggleFanStatus(): void {
     this.fans.status = !this.fans.status;
+
+    if (this.fans.status) {
+      this.rotateFan(360);
+    } else {
+      this.rotateFan(0);
+    }
+  }
+
+  private rotateFan(degrees: number): void {
+    this.renderer.setStyle(this.ceilingContainer.nativeElement, 'transform', `rotate(${degrees}deg)`);
+  }
+  
+  toggleSnowflake() {
+    this.clims.status = !this.clims.status;
+
+    // You can remove the following lines if ngClass is handling the classes for you
+    if (this.clims.status) {
+      this.renderer.addClass(this.snowflakeIcon.nativeElement, 'fa-spin');
+      this.renderer.addClass(this.snowflakeIcon.nativeElement, 'big_on');
+    } else {
+      this.renderer.removeClass(this.snowflakeIcon.nativeElement, 'fa-spin');
+      this.renderer.removeClass(this.snowflakeIcon.nativeElement, 'big_on');
+    }
   }
 }
 
